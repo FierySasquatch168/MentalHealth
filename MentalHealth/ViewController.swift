f0a2ef927008f0bca4c4a3da0a0d57d1655f6719
FierySasquatch168 <9120603@gmail.com>
2023-02-07 11:29:27 +0200

    Initial Commit

---
+++ MentalHealth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MentalHealth
+//
+//  Created by Aleksandr Eliseev on 07.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
